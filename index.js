a8acf232a29e7f6968bf054b6744d6927e46a1bc
Sugiyama Yoshiyuki <yo.sugiyama67@gmail.com>
2020-12-06 11:15:14 +0900

    バリデーションチェックの一部追加。

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 var app = new Vue({
     el: '#app',
     data: {
-      results: [],
-      number: 0,
       input_data_size: "",
       select_data_size : "b",
-      //select_data2 : "1"
+      error: {
+        require: false,
+        noNumber: false
+      }
     },
     methods: {
+      require(value){
+        this.error.require = (value.length < 1) ? true : false;
+      },
+      validationCheck(value){
+        require_result = this.require(value);
+
+        // エラーの場合は他のチェックをする前に返す
+        if (!require_result) return false
+
+        // TODO チェック内容
+        // ,区切りのデータも許容する予定
+        // 数値以外は弾く
+        
+        
+        return true
+      },
+        // input_size: function () {
+        //   this.error.noNumber = ((this.input_size.length < 1) && (isFinite(this.input_size))) ? true : false;
+        // }
       calc: function(){
         // まず値のチェックを実施する。
-        // select_data_sizeで選択されたデータサイズから各サイズへ変換し、その結果をテキストボックスに入力する。
+        result = this.validationCheck(this.input_data_size);
+
+        if (require_result){
+          // 計算処理を実行する
+          // select_data_sizeで選択されたデータサイズから各サイズへ変換し、その結果をテキストボックスに入力する。
+
+        }
+
+
+        
       },
       clear: function(){
         this.input_data_size = ""
