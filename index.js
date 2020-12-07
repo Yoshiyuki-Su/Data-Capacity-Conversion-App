21279df3959d79332b347d0769436f6a8e6d5942
Sugiyama Yoshiyuki <yo.sugiyama67@gmail.com>
2020-12-07 22:34:26 +0900

    計算処理の一部作成。

--- index.js
+++ index.js
@@ -28,16 +28,6 @@ Vue.component('ValidationObserver', ValidationObserver);
 const VueInputAutowidth = window['VueInputAutowidth'];
 Vue.use(VueInputAutowidth);
 
-{/* <ol>バイト<input type="text" v-model="text_data1">(B)</ol>
-<ol>キロバイト<input type="text" v-model="text_data2">(KB)</ol>
-<ol>メガバイト<input type="text" v-model="text_data3">(MB)</ol>
-<ol>ギガバイト<input type="text" v-model="text_data4">(GB)</ol>
-<ol>テラバイト<input type="text" v-model="text_data5">(TB)</ol>
-<ol>ペタバイド<input type="text" v-model="text_data6">(PB)</ol>
-<ol>エクサバイト<input type="text" v-model="text_data7">(EB)</ol>
-<ol>ゼタバイト<input type="text" v-model="text_data8">(ZB)</ol>
-<ol>ヨタバイト<input type="text" v-model="text_data9">(YB)</ol> */}
-
 
 var InputText = Vue.extend({
   props: ['data'],
@@ -45,7 +35,7 @@ var InputText = Vue.extend({
   <div class="container">
     <table>
       <tr>
-        <td style="float: right;"><ol>{{ data.size }}</ol></td>
+        <td class="text-right"><ol>{{ data.size }}</ol></td>
         <td><ol>
             <input type="text" v-model="data.result_data"
             v-autowidth="{maxWidth: '300px', minWidth: '10px', comfortZone: 0}">{{ data.unit }}</ol>
@@ -63,17 +53,53 @@ var app = new Vue({
     el: '#app',
     data: {
       input_data_size: "",
-      select_data_size : "b",
-      result_data: "aa",
+      select_data_size : "Byte",
+      // result_data: "aa",
+      select_data_digits: "10",
       results: [
         {
-          size: "size1",
-          unit: "b",
+          size: "バイト",
+          unit: "(B)",
           result_data: "aaaaa",
         },
         {
-          size: "size2",
-          unit: "kb",
+          size: "キロバイト",
+          unit: "(KB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "メガバイト",
+          unit: "(MB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "ギガバイト",
+          unit: "(GB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "テラバイト",
+          unit: "(TB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "ペタバイド",
+          unit: "(PB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "エクサバイト",
+          unit: "(EB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "ゼタバイト",
+          unit: "(ZB)",
+          result_data: "vvvvv",
+        },
+        {
+          size: "ヨタバイト",
+          unit: "(YB)",
           result_data: "vvvvv",
         },
       ]
@@ -82,10 +108,50 @@ var app = new Vue({
       calc: function(){
         // 計算処理を実行する
         // select_data_sizeで選択されたデータサイズから各サイズへ変換し、その結果をテキストボックスに入力する。
+        
+        // 入力値を取得
+        this.input_data_size
+        // 入力値の単位を取得
+        this.select_data_size
+        // 出力結果の桁数を取得
+        this.select_data_digits
+
+        // 下記を参考に計算結果をいれる
+        // this.results[0].result_data = "abcdefg"
+        // 9パターン
+
+
+        // TODO まず下記を使うならカンマを変換してやらないといけない
+        //使用例
+        var fileSize = 123456789;
+
+        var formatSize = this.byteFormat(fileSize, 3);
+        console.log(formatSize);
+
       },
       clear: function(){
         this.input_data_size = ""
-      }
+      },
+      byteFormat: function(number, digits) {
+        const k = 1024
+
+        var bytes  = Number(number),
+            suffix = ['Byte', 'KB', 'MB', 'GB', 'TB', 'PB', 'ZB', 'YB'],
+            target = Math.floor(Math.log(bytes) / Math.log(k)),
+            unit   = suffix[target];
+        
+        console.log(target);
+        console.log(unit);
+
+        console.log(Math.pow(k, Math.floor(digits)))
+        console.log((bytes / Math.pow(k, Math.floor(digits))).toFixed(digits) + ' ' + unit)
+        console.log((bytes / Math.pow(k, Math.floor(digits))).toExponential(digits) + ' ' + unit)
+
+        return parseFloat((bytes / Math.pow(k, target)).toFixed(digits)) + ' ' + suffix[target];
+        //return (bytes / Math.pow(1024, Math.floor(digits))).toFixed(digits) + ' ' + unit;
+        //return (bytes / Math.pow(1024, Math.floor(digits))).toExponential(digits) + ' ' + unit;
+        
+      },
     }
   })
 
